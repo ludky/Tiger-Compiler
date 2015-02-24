c70d6c6acc3a9ea77037fdd28d55ab5849f08877
q22146184 <yangwenduo02335@gmail.com>
2015-02-23 21:10:16 -0500

    Error handling complete

--- Main.java
+++ Main.java
@@ -3,14 +3,33 @@ import org.antlr.runtime.*;
 public class Main {
 	
 	public static void main(String[] args) {
-		try {
-			TigerLexer a = new TigerLexer(new ANTLRFileStream("src/test.tig"));
-			TokenStream tokens = new CommonTokenStream(a);
-			TigerParser parser = new TigerParser(tokens);
-			parser.program();
-			System.out.println("Successfully parsed.");
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (args.length == 0) {
+			throw new IllegalArgumentException();
+		} else if (args.length == 1) {
+			try {
+				String inputFile = args[0];
+				TigerLexer a = new TigerLexer(new ANTLRFileStream(inputFile));
+				TokenStream tokens = new CommonTokenStream(a);
+				TigerParser parser = new TigerParser(tokens);
+				parser.program();
+				System.out.println("Successfully parsed.");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (args.length == 2 && args[0].equals("-debug")) {
+			try {
+				String inputFile = args[1];
+				TigerLexer a = new TigerLexer(new ANTLRFileStream(inputFile));
+				TokenStream tokens = new CommonTokenStream(a);
+				TigerParser parser = new TigerParser(tokens);
+				parser.program();
+				parser.printTokenType();
+				System.out.println("Successfully parsed.");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			throw new IllegalArgumentException();
 		}
 	}
 }
