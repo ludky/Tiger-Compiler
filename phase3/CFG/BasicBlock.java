223caba50df55ef2f04e4e2abc50d13d8ba911d8
Fu(Ludky) <ludky109@hotmail.com>
2015-04-24 11:34:32 -0400

    CFG in progress

--- phase3/CFG/BasicBlock.java
+++ phase3/CFG/BasicBlock.java
@@ -2,15 +2,32 @@ import java.util.*;
 
 public class BasicBlock{
 
+    private int id;
     private List<String> block;
-    private List<BasicBlock> nextBlocks;
-    private BasicBlock prevBlock;
+    private List<BasicBlock> nextBlock;
+    //private BasicBlock prevBlock;
     private List<streamInOut> instructions;
     private List<String> in_var;
     private List<String> out_var;
 
-    public BasicBlock(List<String> block) {
-	
+    public BasicBlock(LinkedList<String> block) {
+	this(block, 0);
     }
+
+    public BasicBlock(LinkedList<String> block, int id) {
+	this.block = block;
+	this.id = id;
+	nextBlock = new LinkedList<BasicBlock>();
+    }
+
+    public LinkedList<BasicBlock> getNext() {
+	return nextBlock;
+    } 
+
+    public void setBlockId(int id) {
+	this.id = id;
+    }
+
+    public 
 
 }
